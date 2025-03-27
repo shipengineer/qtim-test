358bb1acd29fda554bfc25f349693b853adb71c7
Mysnik Artem <112334992+shipengineer@users.noreply.github.com>
2025-03-27 16:20:07 +0300

    add libs and folders

--- qtim-test/nuxt.config.ts
+++ qtim-test/nuxt.config.ts
@@ -3,10 +3,32 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 
+  tailwindcss: {
+    config: {
+      theme: {
+        colors: {
+          aquamarine: {
+            1: '#9EFFBF'
+          },
+          black: '#101010',
+          blue: { 1: '#1059C8' },
+          gray: {
+            1: '#232323',
+            2: '#868686',
+            3: '#F3F3F3',
+          },
+          violet: { 1: '#7941A7', 2: '#E2BEFF' },
+
+          white: '#FFFFFF',
+        }
+      }
+    }
+  },
   modules: [
     '@nuxt/eslint',
     '@nuxt/icon',
     '@nuxt/image',
-    '@nuxt/fonts'
+    '@nuxt/fonts',
+    '@nuxtjs/tailwindcss',
   ]
 })
